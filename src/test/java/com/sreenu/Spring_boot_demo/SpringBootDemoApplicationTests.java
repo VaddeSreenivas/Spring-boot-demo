3a85fe8239c5f47c80f098e6979b0739a716aca4
VaddeSreenivas <vsreenivasm@gmail.com>
2025-07-31 17:51:19 +0530

    Spring_Boot Demo Project

---
+++ src/test/java/com/sreenu/Spring_boot_demo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreenu.Spring_boot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
